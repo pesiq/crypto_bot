772b7d3196d1d703552fa1b183a520f6f967ddee
Ruslan Mirziianov <rmir212121@gmail.com>
2023-09-27 16:23:26 +0300

    Added theme-provider and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Test from './test'
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Test />
-      ...
-    </main>
+
+export default function Home(){
+
+
+  return(
+    <>
+      <div>
+        benis
+      </div>
+    </>
   )
 }
