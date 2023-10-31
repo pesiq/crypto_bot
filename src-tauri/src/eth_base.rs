aa96855fd9e402d35854f4d910de4eb7f1355ef0
Ruslan Mirziianov <rmir212121@gmail.com>
2023-10-31 11:25:55 +0300

    Preparing for swap module

--- src-tauri/src/eth_base.rs
+++ src-tauri/src/eth_base.rs
@@ -3,7 +3,31 @@
 use tauri::Manager;
 use web3;
 
+    /*
+    Need to make generic trait fr account to login to acc
+    sign and approve tx
+     */
+
+    /*
+    slippage 
+    if high can get less tokens bo % of slippage
+    if low transaction can fail
+    */
+
+    /*
+        get contract
+        get pool
+
+        do swap(from, to, min/max amount, )
+        
+     */
+
 #[tarui::command]
 fn swap(){
 
+
+
+
+
+    //get contract
 }
