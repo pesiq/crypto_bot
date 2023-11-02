1f41e4aab35f1ca8ff7909d8febf6166accd1774
Ruslam Mir <rmir212121@gmail.com>
2023-11-03 01:21:55 +0300

    More swap pseudocode

--- src-tauri/src/modules/eth_base.rs
+++ src-tauri/src/modules/eth_base.rs
@@ -1,7 +1,5 @@
 
 use tauri;
-use web3;
-use super::account;
 
     /*
     Need to make generic trait fr account to login to acc
@@ -14,8 +12,27 @@ use super::account;
     if low transaction can fail
     */
 
+    /* 
+        Module struct/traits/types
+
+        Transaction data: struct
+        all methods in web3::api::Eth
+            chain id = chain_id
+            adress
+            value
+            gas = gas_price
+            nonce = transaction_count
+
+        Swap contract
+            Contract adress
+            Contract ABI
+    
+    */
     /*  
     Module functions and methods
+
+        GetMinimalAmount
+            gets minimal amount for transaction
 
         Estimate gas
             Estimate gas usage for assembled tx
@@ -45,14 +62,47 @@ struct SwapPayload{
 
 }
 
-#[tauri::command]
+fn from_ETH(){
+
+}
+
+fn to_ETH(){
+    
+}
+
+
 fn swap(){
 
+    //get contract
 
+    //check swap coin
+
+    //if from ETH then call ExactETHtoCoin
+    //if from coin then call ExactCointoETH
+
+    // get tx from fucntions
+
+    // send transaction
+
+    //await transaction
 
     //get contract
 }
 
-fn estimate_gas(){
-    
+
+#[tauri::command]
+fn make_swaps(){
+
+    //foreach account assemble swap and call swap
+
+    //assemble swap
+    //swap_contract: struct with contract adress and abi
+    /* 
+     to assemble transaction you need minimum:
+        src
+        dest
+        amount
+        slippage
+
+    */
 }
